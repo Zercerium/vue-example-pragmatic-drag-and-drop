9a86d9c2a6d03b3f3b650e30c0e70b70eeae2b7b
Nick Kotsakidis <zercerium@mailbox.org>
2024-05-12 11:57:40 +0200

    set target es2020

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import nightwatchPlugin from 'vite-plugin-nightwatch'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import nightwatchPlugin from 'vite-plugin-nightwatch';
+import VueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    nightwatchPlugin(),
-    VueDevTools(),
-  ],
+  plugins: [vue(), nightwatchPlugin(), VueDevTools()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  build: { target: 'es2020' }
+});
